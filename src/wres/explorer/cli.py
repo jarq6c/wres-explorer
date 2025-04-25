c92427ed9cbddbba9327aa079e5e78b265003725
jason.regina <jason.regina@noaa.gov>
2025-04-25 18:54:18 +0000

    mockup widgets class

--- src/wres/explorer/cli.py
+++ src/wres/explorer/cli.py
@@ -7,6 +7,71 @@ from .data import DataManager
 pn.extension("tabulator")
 pn.extension("plotly")
 
+class Widgets:
+    """
+    Widgets for the dashboard.
+    
+    Attributes
+    ----------
+    file_selector: pn.widgets.FileSelector
+        File selector widget for selecting CSV files.
+    load_data_button: pn.widgets.Button
+        Button to load/reload data.
+    left_feature_selector: pn.widgets.AutocompleteInput
+        Autocomplete input for selecting left feature.
+    right_feature_selector: pn.widgets.AutocompleteInput
+        Autocomplete input for selecting right feature.
+    map_selector: pn.pane.Plotly
+        Pane for displaying the map of features.
+    description_pane: pn.pane.Markdown
+        Pane for displaying feature descriptions.
+    metrics_table: pn.widgets.Tabulator
+        Table for displaying metrics data.
+    selected_metric: pn.widgets.Select
+        Select widget for selecting metrics.
+    metrics_pane: pn.pane.Plotly
+        Pane for displaying metrics plots.
+    """
+    def __init__(self):
+        self.file_selector = pn.widgets.FileSelector(
+            directory="./",
+            file_pattern="*.csv.gz",
+            only_files=True,
+            value=[]
+        )
+        self.load_data_button = pn.widgets.Button(
+            name="Load/Reload Data",
+            button_type="primary"
+        )
+        self.metrics_table = pn.widgets.Tabulator(
+            pd.DataFrame({"message": ["no data loaded"]}),
+            show_index=False,
+            disabled=True,
+            width=1280,
+            height=720
+        )
+        self.left_feature_selector = pn.widgets.AutocompleteInput(
+            name="LEFT FEATURE NAME",
+            options=[],
+            search_strategy="includes",
+            placeholder=f"Select LEFT FEATURE NAME"
+        )
+        self.right_feature_selector = pn.widgets.AutocompleteInput(
+            name="RIGHT FEATURE NAME",
+            options=[],
+            search_strategy="includes",
+            placeholder=f"Select RIGHT FEATURE NAME"
+        )
+        self.description_pane = pn.pane.Markdown(
+            "LEFT FEATURE DESCRIPTION\n"
+        )
+        self.map_selector = pn.pane.Plotly()
+        self.selected_metric = pn.widgets.Select(
+            name="Select Metric",
+            options=[]
+        )
+        self.metrics_pane = pn.pane.Plotly()
+
 class Dashboard:
     """
     Dashboard for displaying WRES CSV data.
